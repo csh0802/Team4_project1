d37ae8de39235fc1a353491275df8a11efab06d5
csh <tjdgh6273@naver.com>
2021-12-08 11:38:36 +0900

    first commit

---
+++ src/main/java/com/example/demo/Team4Project1Application.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Team4Project1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Team4Project1Application.class, args);
+	}
+
+}
